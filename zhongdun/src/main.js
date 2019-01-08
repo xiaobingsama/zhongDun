0dce564d6528923d6e74db73bd7fe628a337d396
ChenXinHong <604083820@qq.com>
2019-01-08 15:15:05 +0800

    config+vux

--- zhongdun/src/main.js
+++ zhongdun/src/main.js
@@ -3,12 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import iView from 'iview'
-import 'iview/dist/styles/iview.css'
-
-Vue.use(iView);
+import vux from 'vux'
+import fastclick from 'fastclick'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+fastclick.attach(document.body)
+
+Vue.prototype.$ajax = axios;
 
 /* eslint-disable no-new */
 new Vue({
