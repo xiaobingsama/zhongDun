3e457d1ee6296c1954f135a926ddf4d98134ea13
zstart <845907306@qq.com>
2019-01-08 18:03:07 +0800

    home 模块
    布局

--- zhongdun/src/pages/home/home.vue
+++ zhongdun/src/pages/home/home.vue
@@ -3,14 +3,38 @@
     <div class="banner"></div>
     <div class="module_block">
       <div class="module_block_list">
-         <router-link to="/enter" class="module_block_a">About</router-link>
-         <router-link to="/enter" class="module_block_a">About</router-link>
-         <router-link to="/enter" class="module_block_a">About</router-link>
-         <router-link to="/enter" class="module_block_a">About</router-link>
-         <router-link to="/enter" class="module_block_a">About</router-link>
-         <router-link to="/enter" class="module_block_a">About</router-link>
-         <router-link to="/enter" class="module_block_a">About</router-link>
-         <router-link to="/enter" class="module_block_a">About</router-link>
+         <router-link to="/enter" class="module_block_a">
+            <img src="../../images/banner.png" alt="">
+            <p>在线报名</p>
+         </router-link>
+         <router-link to="/enter" class="module_block_a">
+            <img src="../../images/banner.png" alt="">
+            <p>在线报名</p>
+         </router-link>
+         <router-link to="/enter" class="module_block_a">
+            <img src="../../images/banner.png" alt="">
+            <p>在线报名</p>
+         </router-link>
+         <router-link to="/enter" class="module_block_a">
+            <img src="../../images/banner.png" alt="">
+            <p>在线报名</p>
+         </router-link>
+         <router-link to="/enter" class="module_block_a">
+            <img src="../../images/banner.png" alt="">
+            <p>在线报名</p>
+         </router-link>
+         <router-link to="/enter" class="module_block_a">
+            <img src="../../images/banner.png" alt="">
+            <p>在线报名</p>
+         </router-link>
+         <router-link to="/enter" class="module_block_a">
+            <img src="../../images/banner.png" alt="">
+            <p>在线报名</p>
+         </router-link>
+         <router-link to="/enter" class="module_block_a">
+            <img src="../../images/banner.png" alt="">
+            <p>在线报名</p>
+         </router-link>
       </div>
     </div>
     <div class="module_block hot_module">
@@ -21,63 +45,60 @@
         <router-link to="/enter" class="more_link">全部></router-link>
       </div>
       <div class="module_block_list hot_module_list">
-        <router-link to="/enter" class="hot_module_a">About</router-link>
-        <router-link to="/enter" class="hot_module_a">About</router-link>
-        <router-link to="/enter" class="hot_module_a">About</router-link>
+        <router-link to="/enter" class="hot_module_a">
+            <p>在线报名</p>
+           <img src="../../images/banner.png" alt="">
+        </router-link>
+        <router-link to="/enter" class="hot_module_a">
+            <p>在线报名</p>
+           <img src="../../images/banner.png" alt="">
+        </router-link>
+         <router-link to="/enter" class="hot_module_a">
+            <p>在线报名</p>
+           <img src="../../images/banner.png" alt="">
+        </router-link>
       </div>
     </div>
      <div class="module_block">
-      <tab :line-width="1" active-color='#5ebf83' :custom-bar-width="getBarWidth" v-model="index">
+      <tab :line-width="1" active-color='#5ebf83' custom-bar-width="50px" >
         <tab-item selected @on-item-click="onClick('news')">推荐</tab-item>
-        <tab-item @on-item-click="onClick('')">课程精选</tab-item>
-        <tab-item>免费试听</tab-item>
+        <tab-item @on-item-click="onClick('course')">课程精选</tab-item>
+        <tab-item  @on-item-click="onClick('video')">免费试听</tab-item>
       </tab>
-      <div class="grid-demo" v-if="active_tab=='idemo'">
+      <div class="grid-demo" v-if="active_tab=='news'">
+        <ul>
+          <li>qq</li>
+        </ul>
       </div>
-      <div class="novice-demo" v-if="active_tab=='vile'">
+      <div class="novice-demo" v-if="active_tab=='course'">
+        kc
+      </div>
+       <div class="novice-demo" v-if="active_tab=='video'">
+        sp
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { Tab, TabItem, Sticky, Divider, XButton, Swiper, SwiperItem } from 'vux'
-import news from '@/page/home/news'
-const list = () => ['推荐', '课程精选', '免费试听']
+import { Tab, TabItem} from 'vux'
+
 
 export default {
   components: {
     Tab,
     TabItem,
-    Sticky,
-    Divider,
-    XButton,
-    Swiper,
-    SwiperItem
   },
   data () {
     return {
-      index01: 0,
-      list2: list(),
-      demoDisabled: 'A',
-      index: 0,
-      getBarWidth: function (index) {
-        return (index + 1) * 22 + 'px'
-      }
+      active_tab: 'news',
     }
   },
   methods:{
-    change:function(x){     
-      for(var i=0;i<this.active.length;i++){
-          this.active[i]=false;
-          this.active[x]=true;
-          this.who=this.tab[x].func;
+    onClick(state){
+        this.active_tab = state;
       }
-      console.log(this.active);
-      // console.log(x);
-      this.$set(this.active, 3, 0);
-    }
-  }
+    },
 }
 </script>
 
@@ -105,20 +126,33 @@ export default {
     height: 100%;
     position: relative;
     margin-bottom: 1rem;
-   
+    padding-bottom: 1rem;
+
    }
    .module_block_a{
     width: 25%;
-    padding: .3rem 0;
-    display: -ms-flexbox;
-    display: flex;
+    padding-top: 1rem;
+   /* display: -ms-flexbox;
+    display: flex;*/
     -ms-flex-pack: center;
     justify-content: center;
+   }
+   .module_block_a img{
+    display: block;
+    width: 3rem;
+    height: 3rem;
+    margin: 0 auto;
+   }
+   .module_block_a p{
+    font-size: .8rem;
+    color: #343434;
+    line-height: 1.5rem;
    }
   .hot_title{
     padding: 10px 1rem;
     border-bottom: 1px solid #e9e9e9;
     line-height: 1.5rem;
+    text-align: left;
   }
   .hot_title h2{
     display: inline-block;
@@ -130,10 +164,17 @@ export default {
    .hot_module_a{
     width: 33.33%;
     padding: .3rem 0;
-    display: -ms-flexbox;
-    display: flex;
-    -ms-flex-pack: center;
-    justify-content: center;
+   }
+   .hot_module_a img{
+      display: block;
+      width: 4rem;
+      height: 4rem;
+      margin: 0 auto;
+   }
+   .hot_module_a p{
+    font-size: 1rem;
+    color: #343434;
+    line-height: 2.5rem;
    }
    .more_link{
     float: right;
